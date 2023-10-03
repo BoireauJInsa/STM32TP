3efcb4895d870f6333c2b66ca6739c1bcc40a2f8
ThibaultGautheron <tgauther@insa-toulouse.fr>
2023-10-03 13:39:44 +0200

    tp

--- MesDrivers/Source/Driver_GPIO.c
+++ MesDrivers/Source/Driver_GPIO.c
@@ -26,3 +26,15 @@ void MyGPIO_Init (MyGPIO_Struct_TypeDef * GPIOStructPtr) {
 			
 RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4);
 }
+
+int MyGPIO_Read(GPIO_TypeDef * GPIO, char GPIO_Pin){
+	return GPIO->IDR & (1<<GPIO_Pin);
+}
+
+void MyGPIO_Set(GPIO_TypeDef * GPIO, char GPIO_Pin){
+	GPIO->ODR |= (1<<GPIO_Pin);
+}
+void MyGPIO_Reset(GPIO_TypeDef * GPIO, char GPIO_Pin){
+	GPIO->ODR &= ~(1<<GPIO_Pin);
+}
+void MyGPIO_Toggle(GPIO_TypeDef * GPIO, char GPIO_Pin);
